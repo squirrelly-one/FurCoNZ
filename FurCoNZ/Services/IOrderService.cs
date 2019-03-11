b79b5d190585d9e38f56a47d0fa185ebb39533d5
TC Foxtaur <tcfox@tc.nz>
2019-03-11 23:52:06 +1300

    Create initial OrderService interface

---
+++ FurCoNZ/Services/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FurCoNZ.Models;
+
+namespace FurCoNZ.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<TicketType>> GetTicketTypes(bool IncludeUnavailableTickets = true, CancellationToken cancellationToken = default);
+        Task<DateTimeOffset> ReserveTicketsForPurchase(IDictionary<int, int> ticketsToReserveById, CancellationToken cancellationToken = default);
+        Task<Order> CreatePendingOrder(User purchasingAccount, IEnumerable<Ticket> ticketsInBasket, CancellationToken cancellationToken = default);
+    }
+}
