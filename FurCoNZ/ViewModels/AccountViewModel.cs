ee5a4111ccdfb259556f1da555517ee002f2cfde
NZSmartie <nzsmartie@gmail.com>
2019-03-22 00:06:30 +1300

    Rename Profile to Account

---
+++ FurCoNZ/ViewModels/AccountViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FurCoNZ.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string CalledName { get; set; }
+    }
+}
