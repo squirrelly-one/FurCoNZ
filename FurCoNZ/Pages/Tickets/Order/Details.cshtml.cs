771d0750827dd3f61763ba8d94338820915855f3
TC Foxtaur <tcfox@tc.nz>
2019-03-22 00:17:12 +1300

    Moved Order to it’s own subdirectory, added two more pages for ticket flow, added EF for Ticket Types on order page

---
+++ FurCoNZ/Pages/Tickets/Order/Details.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FurCoNZ.Pages.Tickets.Order
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnPost()
+        {
+        }
+    }
+}
