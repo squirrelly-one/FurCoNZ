917c1175254bf4d321d01ac1f28564fa7e5418b0
TC Foxtaur <tcfox@tc.nz>
2019-04-07 19:14:35 +1200

    Flesh out the ticket details form with some fields

--- FurCoNZ/ViewModels/TicketDetailViewModel.cs
+++ FurCoNZ/ViewModels/TicketDetailViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace FurCoNZ.ViewModels
+﻿using System;
+using FurCoNZ.Models;
+
+namespace FurCoNZ.ViewModels
 {
-    internal class TicketDetailViewModel
+    public class TicketDetailViewModel
     {
+        public int Id { get; set; }
+        public TicketType TicketType { get; set; } // TODO: Probably need to map this to a ViewModel
+        public string EmailAddress { get; set; }
+        public bool SendTicketToOtherAccount { get; set; }
+        public string BadgeName { get; set; }
+
+        // https://www.w3.org/International/questions/qa-personal-names
+        public string PreferredFullName { get; set; }
+        public string PreferredKnownAs { get; set; }
+
+        // Legal name is required for legal reasons, however this can be distressing in some circumstances.
+        // We need to ensure this is never publically used, and is only available to:
+        // * FurcoNZ Staff, where necessary (such as registration)
+        // * NZ officials, when legally required
+        // * Where required for safety reasons (such as medical emergencies)
+        public string LegalFullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAllergies { get; set; }
+        public string OtherNotes { get; set; }
     }
 }
