2dc24399f0d791addbbc5a08999a98ec7e0306d5
NZSmartie <nzsmartie@gmail.com>
2019-04-12 00:52:30 +1200

    Support initialising AccountVM with User model

--- FurCoNZ/ViewModels/AccountViewModel.cs
+++ FurCoNZ/ViewModels/AccountViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FurCoNZ.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,11 +9,32 @@ namespace FurCoNZ.ViewModels
 {
     public class AccountViewModel
     {
+        public AccountViewModel() { }
+
+        public AccountViewModel(User user)
+        {
+            Name = user.Name;
+            Email = user.Email;
+            Pronouns = user.Pronouns;
+            Allergies = user.Allergies;
+            DietryRequirements = user.DietryRequirements;
+            DateOfBirth = user.DateOfBirth;
+        }
+
         [Required]
         public string Name { get; set; }
 
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        public string Pronouns { get; set; }
+
+        public string Allergies { get; set; }
+
+        public string DietryRequirements { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
     }
 }
