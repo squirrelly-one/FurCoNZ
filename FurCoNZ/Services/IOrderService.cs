1a8ea8df1fc86387900ebd4bb22e0275d7b4d280
TC Foxtaur <tcfox@tc.nz>
2019-05-19 13:20:09 +1200

    Reorder interface members

--- FurCoNZ/Services/IOrderService.cs
+++ FurCoNZ/Services/IOrderService.cs
@@ -8,19 +8,13 @@ namespace FurCoNZ.Services
 {
     public interface IOrderService
     {
-        Task<IEnumerable<TicketType>> GetTicketTypesAsync(bool IncludeUnavailableTickets = true, CancellationToken cancellationToken = default);
-        Task<DateTimeOffset> ReserveTicketsForPurchaseAsync(IDictionary<int, int> ticketsToReserveById, CancellationToken cancellationToken = default);
-        Task<Order> CreateOrderAsync(User purchasingAccount, IEnumerable<Ticket> ticketsInBasket, CancellationToken cancellationToken = default);
-
-
-        Task<IEnumerable<Order>> GetUserOrders(User user, CancellationToken cancellationToken = default);
-
-        Task<Order> GetUserOrderAsync(User user, int orderId, CancellationToken cancellationToken = default);
-
-        Task<Order> GetUserPendingOrderAsync(User user, CancellationToken cancellationToken = default);
-
         Task AddReceivedFundsForOrder(int orderId, int amountCents, string paymentProvider, string paymentReference, DateTimeOffset when, CancellationToken cancellationToken = default);
-
+        Task<Order> CreateOrderAsync(User purchasingAccount, IEnumerable<Ticket> ticketsInBasket, CancellationToken cancellationToken = default);
+        Task<IEnumerable<TicketType>> GetTicketTypesAsync(bool IncludeUnavailableTickets = true, CancellationToken cancellationToken = default);
+        Task<Order> GetUserOrderAsync(User user, int orderId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Order>> GetUserOrders(User user, CancellationToken cancellationToken = default);
+        Task<Order> GetUserPendingOrderAsync(User user, CancellationToken cancellationToken = default);
         Task RefundFundsForOrder(int orderId, int amountCents, string paymentProvider, string paymentReference, DateTimeOffset when, CancellationToken cancellationToken = default);
+        Task<DateTimeOffset> ReserveTicketsForPurchaseAsync(IDictionary<int, int> ticketsToReserveById, CancellationToken cancellationToken = default);
     }
 }
