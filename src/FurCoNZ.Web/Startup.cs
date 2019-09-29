e0a2a44e1473def27fd226381fbeb97602ed9022
TC Foxtaur <tcfox@tc.nz>
2019-09-29 18:45:11 +1300

    Allow Developer Exception Page on Testing

--- src/FurCoNZ.Web/Startup.cs
+++ src/FurCoNZ.Web/Startup.cs
@@ -253,7 +253,7 @@ namespace FurCoNZ.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Testing"))
             {
                 app.UseDeveloperExceptionPage();
             }
