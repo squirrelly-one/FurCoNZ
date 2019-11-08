139c47707d8b06c4b8afb9b3ccde8cb29bbde2cc
NZSmartie <nzsmartie@gmail.com>
2019-11-09 01:13:57 +1300

    Support URLs in emails sent without MVC Router

---
+++ src/FurCoNZ.Web/NullRouter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Routing;
+
+namespace FurCoNZ.Web
+{
+    /// <summary>
+    /// I needed a null router to pass onto the ActionContext for Email templates
+    /// Copied from https://github.com/aspnet/AspNetCore/blob/v2.2.7/src/Http/Routing/src/Internal/NullRouter.cs
+    /// </summary>
+    internal class NullRouter : IRouter
+    {
+        public static readonly NullRouter Instance = new NullRouter();
+
+        private NullRouter()
+        {
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            return null;
+        }
+
+        public Task RouteAsync(RouteContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/FurCoNZ.Web/Services/ViewRenderService.cs
+++ src/FurCoNZ.Web/Services/ViewRenderService.cs
@@ -1,9 +1,10 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -11,7 +12,10 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace FurCoNZ.Web.Services
 {
@@ -26,17 +30,34 @@ namespace FurCoNZ.Web.Services
         private readonly IRazorViewEngine _razorViewEngine;
         private readonly ITempDataProvider _tempDataProvider;
         private readonly IServiceProvider _serviceProvider;
+        private readonly IConfiguration _configuration;
+        private readonly HttpRequestFeature _defaultRequestFeature;
 
         public ViewRenderService(
             IHttpContextAccessor httpContextAccessor,
             IRazorViewEngine razorViewEngine,
             ITempDataProvider tempDataProvider,
-            IServiceProvider serviceProvider)
+            IServiceProvider serviceProvider,
+            IConfiguration configuration)
         {
             _httpContext = httpContextAccessor.HttpContext;
             _razorViewEngine = razorViewEngine;
             _tempDataProvider = tempDataProvider;
             _serviceProvider = serviceProvider;
+            _configuration = configuration;
+
+            var emailBaseUrl = _configuration.GetSection("Email:BaseUrl").Get<string>();
+            var url = new Uri(emailBaseUrl);
+            _defaultRequestFeature = new HttpRequestFeature
+            {
+                Headers =
+                {
+                    {"Host", url.Authority }
+                },
+                Scheme = url.Scheme,
+                QueryString = url.Query,
+                Path = url.AbsolutePath,
+            };
         }
 
         public async Task<(string Output, ViewDataDictionary ViewData)> RenderToStringAsync<TModel>(Controller controller, TModel model, bool partial = false)
@@ -80,8 +101,11 @@ namespace FurCoNZ.Web.Services
                 }
                 else
                 {
-                    var defaultHttpContext = new DefaultHttpContext { RequestServices = serviceScope.ServiceProvider, RequestAborted = cancellationToken };
-                    actionContext = new ActionContext(defaultHttpContext, new RouteData(), new ActionDescriptor());
+                    var features = new FeatureCollection();
+                    features.Set<IHttpRequestFeature>(_defaultRequestFeature);
+
+                    var defaultHttpContext = new DefaultHttpContext (features){ RequestServices = serviceScope.ServiceProvider, RequestAborted = cancellationToken };
+                    actionContext = new ActionContext(defaultHttpContext, new RouteData { Routers = { NullRouter.Instance } }, new ActionDescriptor());
                 }
 
                 var viewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary())
